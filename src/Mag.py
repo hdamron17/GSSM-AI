33646f40ec8e230d2e824a25bd46ff529f39845b
Hunter Damron <hdamron1594@yahoo.com>
2017-03-09 13:32:09 -0500

    Added first iterations of multiple files

--- src/Mag.py
+++ src/Mag.py
@@ -5,3 +5,5 @@ DavedMD chatbot logic engine parses inputs and responds accordingly
 * Must have response something if user responds that they've never had any disease
 * If the bot gets exhausted of responses, it will "refer you to another professional" aka restart the bot (possibly with a different name)
 '''
+
+
