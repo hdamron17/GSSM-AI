88c7d64b28e55da25bfe55bd1e55fbdc0f0888a9
Hunter Damron <hdamron1594@yahoo.com>
2017-03-08 23:43:17 -0500

    Some small changes

---
+++ src/Mag.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+''' 
+DavedMD chatbot logic engine parses inputs and responds accordingly
+* Stores information about responses to questions about symptoms
+* Must have response something if user responds that they've never had any disease
+* If the bot gets exhausted of responses, it will "refer you to another professional" aka restart the bot (possibly with a different name)
+'''
