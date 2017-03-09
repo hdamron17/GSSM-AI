f1c6a26a256027b36e1773eb36c5783c10ba1daf
Hunter Damron <hdamron1594@yahoo.com>
2017-03-09 13:32:23 -0500

    Added first iterations of multiple files

---
+++ src/DavedMD.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+''' 
+Runner module for DavedMD diagnostic chatbot
+* creates Mag.py object
+* loops exchanging inputs to the chatbot and displaying chatbot respones
+* uses keywords to determine quit conditions
+'''
