064429763d680b14400c2b7b77cbb328a51e890f
roboticsteam <jjohnson18@gssm.k12.sc.us>
2017-03-31 16:59:11 -0400

    Works for chars, words, sentences

--- src/wordstat.py
+++ src/wordstat.py
@@ -83,10 +83,55 @@ class ExtensionDelimitedCount(Extension):
 
 class ExtensionWordCount(ExtensionDelimitedCount):
     def init(self):
+        super().init()
         self.key = "words"
         self.delimiter = '.!? \t\n\r\f\v'#matches line-ending punctuation and whitespace characters
         self.misc_punctuation = '(){}<>[]$\'\"' #matches puntuation marks which could possibly be placed conveniently beside a delimiter but not signify a new word
         self.punctuating = False #set to true when previous character is some sort of puntuation (so that something like "what." doesn't match two words '"what' and '"')
+
+class ExtensionSentenceCount(ExtensionDelimitedCount):
+    def init(self):
+        super().init()
+        self.key = "sentences"
+        self.delimiter = '.!?'#matches line-ending punctuation and whitespace characters
+        self.misc_punctuation = '(){}<>[]$\'\" \t\n\r\f\v' #matches puntuation marks which could possibly be placed conveniently beside a delimiter but not signify a new word
+        self.punctuating = False #set to true when previous character is some sort of puntuation (so that something like "what." doesn't match two words '"what' and '"')
+
+class ExtensionSyllableCount(Extension):
+    def init(self):
+        self.key = "syllables"
+
+    def count(self, character):
+        pass #TODO count syllables by character
+
+class WordStat():
+    def __init__(self, *extensions):
+        '''
+        Constructs a WordStat object to start counting stats of a text
+        :param extensions: multiple Extension classes (not initialized)
+        '''
+        self.extensions = [ext() for ext in extensions]
+        self.reset()
+
+    def reset(self):
+        for ext in self.extensions:
+            ext.reset()
+
+    def analyze(self, document):
+        '''
+        Analyzes a text document and calculates its statistics
+        :param document: file object to read through
+        :return: returns a dictionary with analysis data
+            May have keys "words", "sentences", "syllables", etc.
+        '''
+        character = document.read(1)
+        while character != '':
+            for ext in self.extensions:
+                ext.count(character.lower()) #count for each couting extension
+            character = document.read(1)
+        for ext in self.extensions:
+            ext.count(character) #count one last time with the empty character to finish words, etc.
+        return merge_dicts([single.get() for single in self.extensions])
 
 def merge_dicts(dicts):
     '''
@@ -103,6 +148,6 @@ def merge_dicts(dicts):
 
 if __name__ == '__main__':
     counter = WordStat(ExtensionCharCount, ExtensionWordCount, ExtensionSentenceCount, ExtensionSyllableCount)
-    text = StringIO("I would walk 500 miles. And I would walk 500 more.")
+    text = StringIO("I would walk 500 miles. And I\nwould walk 500 more.\nHe said, \"Hi.\" And I said, \"I don't wanna talk to you no more!\"")
     output = counter.analyze(text)
     print(output)
