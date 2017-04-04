9b5fd3a08dcc835bb70a48b0c27e949b7d5b2945
roboticsteam <jjohnson18@gssm.k12.sc.us>
2017-04-04 14:05:10 -0400

    tion doesn't work and neither do syllables in general

--- src/wordstat.py
+++ src/wordstat.py
@@ -12,9 +12,6 @@ class Extension():
         '''
         self.reset()
 
-    def view(self):
-        return self.visual
-
     def reset(self):
         '''
         Outer reset does any extension-independent initialization and calls reset()
@@ -100,9 +97,72 @@ class ExtensionSentenceCount(ExtensionDelimitedCount):
 class ExtensionSyllableCount(Extension):
     def init(self):
         self.key = "syllables"
+        self.prev = '' #previous letter
+        self.vowels = "aeiouy" #all vowels to watch for
+        self.word_count = ExtensionWordCount() #word counter because word ending signals end of syllable
+        self.tion_pos = -1 #keeps track of position in "tion" - common exception (starts -1 because 0 is first index)
+        self.tion = "tion" #full tion string to index
+        self.vowel_y = False #becomes true when y is preceded by a vowel so if next is vowel, it is another syllable
+        self.lone_e = False #becomes true when e follows a non-vowel so silent e can be detected
+
+        ### Syllable rules
+        # A consecutive series of vowels is a syllable unless
+        #   o follows any vowel
+        #   a follows u or i
+        #   there are vowels on both sides of y
+        # e on the end of a word is silent
+        # "tion" is a single syllable
 
     def count(self, character):
-        pass #TODO count syllables by character
+        new_word = self.word_count.count(character) #pass the character on
+
+        if new_word and self.lone_e:
+            #subtract one because this is the end and the previous e was actually silent
+            self.total -= 1
+            print('{*}', end='') #TODO remove
+        elif character == 'o' and self.prev in self.vowels and self.prev != 'o':
+            # o follows non-o vowel so syllable
+            self.total += 1
+            print('|', end='') #TODO remove
+        elif character == "a" and self.prev in "ui":
+            # a follows u or i so syllable
+            self.total += 1
+            print('|', end='') #TODO remove
+        elif self.vowel_y and character in self.vowels:
+            # vowel following y is syllable
+            self.total += 1
+            print('|', end='') #TODO remove
+        elif character in self.vowels and self.prev not in self.vowels:
+            # vowel not preceded by a vowel
+            self.total += 1
+            print('|', end='') #TODO remove
+
+        # Statements which affect
+        if character == 'y' and self.prev in self.vowels:
+            # this is a y following a value so the next may be another syllable even though it's multiple vowels in a row
+            self.vowel_y = True
+        else:
+            # reset vowel_y counter
+            self.vowel_y = False
+
+        if character == 'e' and self.prev not in self.vowels:
+            # e not after a vowel is could be silent
+            self.lone_e = True
+        else:
+            # reset lone_e counter
+            self.lone_e = False
+
+        if character == self.tion[self.tion_pos+1] and (self.prev == self.tion[self.tion_pos] if self.tion_pos > 0 else True):
+            # this is the next letter in the tion sequence
+            self.tion_pos += 1
+            if self.tion_pos >= 3:
+                #the tion sequence is complete and a syllable was over counted
+                self.total -= 1
+                print('{*}', end='') #TODO remove
+
+        print(character, end='') #TODO remove
+
+        self.prev = character if not new_word else '' #update the previous character or cast any new word to empty
 
 class WordStat():
     def __init__(self, *extensions):
@@ -145,9 +205,10 @@ def merge_dicts(dicts):
     for single in dicts:
         result.update(single)
     return result
-
+11 + 10 + 5
 if __name__ == '__main__':
     counter = WordStat(ExtensionCharCount, ExtensionWordCount, ExtensionSentenceCount, ExtensionSyllableCount)
-    text = StringIO("I would walk 500 miles. And I\nwould walk 500 more.\nHe said, \"Hi.\" And I said, \"I don't wanna talk to you no more!\"")
+    text = StringIO("I would walk five hundred miles. And I\nwould walk five hundred more.\nHe said, \"Hi.\" And I said, \"I don't wanna talk to you no more, papaya!\"")
+    text = StringIO("My imagination says papaya in the fourth degree of ion riddance")
     output = counter.analyze(text)
-    print(output)
+    print('\n' + str(output)) #TODO take out the newline
