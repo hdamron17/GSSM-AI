c9a6a197bce55c99a7484f8c33e0b0ff1313c96a
Hunter Damron <hdamron1594@yahoo.com>
2019-04-08 15:48:15 -0400

    Added coala with pep8 and space consistency checking

--- src/imgene.py
+++ src/imgene.py
@@ -20,23 +20,28 @@ import warnings
 
 
 warnings.filterwarnings("ignore")
-PROJECT_ROOT =  pathjoin(dirname(abspath(sys.argv[0])), "..")
+PROJECT_ROOT = pathjoin(dirname(abspath(sys.argv[0])), "..")
+
 
 def load_img(filename, greyscale=True):
-    im = cv2.imread(filename, 0 if greyscale else -1) #greyscale else unchanged
+    # greyscale else unchanged
+    im = cv2.imread(filename, 0 if greyscale else -1)
     if im is None:
         raise UserWarning("File not found %s" % filename)
     else:
         return im
 
+
 def random_img(size):
     return np.random.randint(low=0, high=255, size=size)
-    
+
+
 def compare_img(im1, im2):
     total = 0
     for this, that in zip(np.nditer(im1), np.nditer(im2)):
         total += abs(this - that)
     return total
+
 
 def first_gen(model_im, num=20):
     '''
@@ -52,6 +57,7 @@ def first_gen(model_im, num=20):
         gen.append((fitness, rand_im))
     return gen
 
+
 def mate(mom, dad):
     '''
     Mating function between two images (must have same shape)
@@ -64,9 +70,11 @@ def mate(mom, dad):
     shape = mom.shape
     baby = np.zeros(shape)
     for index in np.ndindex(*shape):
-        #tuple index ued to index mom and dad
-        baby[index] = random.choice((mom[index], dad[index])) #replace baby attribute with one from mom or dad
+        # tuple index ued to index mom and dad
+        # replace baby attribute with one from mom or dad
+        baby[index] = random.choice((mom[index], dad[index]))
     return baby
+
 
 def mutate_element(im, rate, std_dev=20):
     '''
@@ -77,12 +85,15 @@ def mutate_element(im, rate, std_dev=20):
     :return: returns image of same shape as im
     '''
     if random.random() <= rate:
-        #this one gets changed
-        return im + random.gauss(0, std_dev) #TODO possibly use a different random for speed
+        # this one gets changed
+        # TODO possibly use a different random for speed
+        return im + random.gauss(0, std_dev)
     else:
-        return im #no change
+        return im  # no change
+
 
 mutate = np.vectorize(mutate_element, excluded=['rate', 'changes'])
+
 
 def mate_and_mutate(mom, dad, mutation_rate, std_dev=20):
     '''
@@ -96,6 +107,7 @@ def mate_and_mutate(mom, dad, mutation_rate, std_dev=20):
     '''
     return mutate(mate(mom, dad), mutation_rate, std_dev)
 
+
 def partition(total, num):
     '''
     Partitions number into as even as possible integers (i.e. partition(20,3) => [6, 7, 7])
@@ -103,9 +115,10 @@ def partition(total, num):
     :param num: integer number of partitions
     :return: returns list of numbers which add to total
     '''
-    base_num = total // num #everything is greater than this
-    extra = total % num #number left over
+    base_num = total // num  # everything is greater than this
+    extra = total % num  # number left over
     return [base_num] * (num-extra) + [base_num+1] * (extra)
+
 
 def evolve(model_im, goal_fitness_per_pixel, pop_size=20, mutation_rate=0.01, std_dev=20, show=False, elite=True, save_freq=-1, log=True):
     '''
@@ -118,54 +131,55 @@ def evolve(model_im, goal_fitness_per_pixel, pop_size=20, mutation_rate=0.01, st
     :return: returns ideal image
     '''
     pop = first_gen(model_im, num=pop_size)
-    pixels = reduce(lambda a,b: a*b, model_im.shape)
+    pixels = reduce(lambda a, b: a * b, model_im.shape)
     goal_fitness = goal_fitness_per_pixel * pixels
-    
+
     if show:
         print("Info: Drawing Initial Screen")
         display = plt.imshow(pop[0][1], cmap="gray", vmin=0, vmax=255)
         plt.ion()
         plt.show()
-        
+
     if log:
         logfile = open(pathjoin(PROJECT_ROOT, "log.csv"), "w+")
         logfile.write("Generation,Fitness\n")
-    
+
     best = min(pop, key=lambda x: x[0])
     generation = 1
-    
+
     if save_freq != -1:
         output_dir = pathjoin(PROJECT_ROOT, "output")
-        
+
         if exists(output_dir):
             rmtree(output_dir)
         makedirs(output_dir)
-    
+
     while best[0] > goal_fitness:
         pop = new_gen(model_im, pop, mutation_rate, std_dev, elite)
-        
+
         best = min(pop, key=lambda x: x[0])
-        
+
         if elite:
             pop.insert(0, best)
-        
-        print("Info: Gen %s Best Fitness = %s" % (generation, best[0] / pixels))
-        
+
+        print("Info: Gen %s Best Fitness = %s" %
+              (generation, best[0] / pixels))
+
         if save_freq != -1 and (generation % save_freq == 0 or generation == 1):
             fname = pathjoin(output_dir, ("%d.png" % generation))
             plt.imsave(fname, best[1], cmap="gray", vmin=0, vmax=255)
-        
+
         if log:
             logfile.write("%s,%s\n" % (generation, best[0] / pixels))
-        
+
         if show and len(plt.get_fignums()) > 0:
-            #If window is still open, it prints
+            # If window is still open, it prints
             try:
                 display.set_data(best[1])
                 plt.draw()
                 plt.pause(0.01)
             except Exception as e:
-                #If anything fails, we just rage quit and kill everything
+                # If anything fails, we just rage quit and kill everything
                 show = False
                 plt.close('all')
                 print("Warning: Window failure with exception %s" % e)
@@ -175,6 +189,7 @@ def evolve(model_im, goal_fitness_per_pixel, pop_size=20, mutation_rate=0.01, st
         logfile.close()
 
     return best[1]
+
 
 def new_gen(model_im, pop, mutation_rate, std_dev, elite):
     '''
@@ -186,14 +201,16 @@ def new_gen(model_im, pop, mutation_rate, std_dev, elite):
     :param elite: if True, it keeps first item
     '''
     pop_size = len(pop)
-    fitnesses = [element[0] for element in pop] #get fitness levels of each
-    
+    fitnesses = [element[0] for element in pop]  # get fitness levels of each
+
     num = pop_size if not elite else pop_size-1
-    new_pop_items = [mate_and_mutate(pop[i][1], pop[j][1], mutation_rate, std_dev) for i,j in pairings(fitnesses, num=num)]
+    new_pop_items = [mate_and_mutate(pop[i][1], pop[j][1], mutation_rate, std_dev)
+                     for i, j in pairings(fitnesses, num=num)]
     new_pop_fitnesses = [compare_img(im, model_im) for im in new_pop_items]
-    
+
     pop = list(zip(new_pop_fitnesses, new_pop_items))
-    return pop   
+    return pop
+
 
 def pairings(fitnesses, num):
     '''
@@ -203,25 +220,29 @@ def pairings(fitnesses, num):
     '''
     assert len(fitnesses) > 1, "Can't make pair of 1 item"
     pairs = []
-    indices = list(range(len(fitnesses))) #get list of possible indices
+    indices = list(range(len(fitnesses)))  # get list of possible indices
     worst = max(fitnesses)
     inversed_fitnesses = worst - fitnesses
     weights = inversed_fitnesses / sum(inversed_fitnesses)
-    
+
     while len(pairs) < num:
-        pairs.append(np.random.choice(indices, size=2, replace=True, p=weights)) #2-tuple with 2 randomly chosen indices
+        # 2-tuple with 2 randomly chosen indices
+        pairs.append(np.random.choice(
+            indices, size=2, replace=True, p=weights))
     return pairs
+
 
 def test_main():
     im = load_img(pathjoin(PROJECT_ROOT, "images", "monalisa.jpg"))
     plt.imshow(im, cmap="gray")
-    #plt.show()
-    
-    recreated = evolve(im, goal_fitness_per_pixel=1, pop_size=20, mutation_rate=0.0002, std_dev=20, show=False, elite=True, save_freq=10, log=True)
+    # plt.show()
+
+    recreated = evolve(im, goal_fitness_per_pixel=1, pop_size=20, mutation_rate=0.0002,
+                       std_dev=20, show=False, elite=True, save_freq=10, log=True)
     plt.ioff()
     plt.imshow(recreated, cmap="gray", vmin=0, vmax=255)
     plt.show()
 
+
 if __name__ == "__main__":
     test_main()
-
